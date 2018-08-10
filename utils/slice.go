a738c6378229bf2eaa291298be4759e6a5b40abd
bambang deny p <bambang.prayitno@kumparan.com>
2018-08-10 13:33:57 +0700

    feature/follow

--- utils/slice.go
+++ utils/slice.go
@@ -36,3 +36,49 @@ func SliceAtoi(s []string) ([]int, error) {
 	}
 	return arr, nil
 }
+
+// Difference :nodoc:
+func Difference(slice1 []string, slice2 []string) []string {
+	var diff []string
+
+	// Loop two times, first to find slice1 strings not in slice2,
+	// second loop to find slice2 strings not in slice1
+	for i := 0; i < 2; i++ {
+		for _, s1 := range slice1 {
+			found := false
+			for _, s2 := range slice2 {
+				if s1 == s2 {
+					found = true
+					break
+				}
+			}
+			// String not found. We add it to return slice
+			if !found {
+				diff = append(diff, s1)
+			}
+		}
+		// Swap the slices, only if it was the first loop
+		if i == 0 {
+			slice1, slice2 = slice2, slice1
+		}
+	}
+
+	return diff
+}
+
+// Uniq :nodoc:
+func Uniq(elements []string) []string {
+	encountered := map[string]bool{}
+
+	// Create a map of all unique elements.
+	for v := range elements {
+		encountered[elements[v]] = true
+	}
+
+	// Place all keys from the map into a slice.
+	result := []string{}
+	for key, _ := range encountered {
+		result = append(result, key)
+	}
+	return result
+}
