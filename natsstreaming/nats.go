8c85d66ebf2989127201dc713d036db8826d697d
Agung Hariadi Tedja <agunghariaditedja@yahoo.com>
2018-11-29 10:46:45 +0700

    Add nats with callback

--- natsstreaming/nats.go
+++ natsstreaming/nats.go
@@ -1,13 +1,22 @@
 package natsstreaming
 
 import (
+	"time"
+
+	"github.com/jpillora/backoff"
 	"github.com/kumparan/go-lib/utils"
 	"github.com/nats-io/go-nats-streaming"
 )
 
 type (
+
+	// EventType :nodoc:
 	EventType string
 
+	// NatsCallback :nodoc:
+	NatsCallback func(conn stan.Conn)
+
+	// NATS :nodoc:
 	NATS struct {
 		conn    stan.Conn
 		testing bool
@@ -33,9 +42,54 @@ func NewNATS(clusterID, clientID, url string) (*NATS, error) {
 	return &NATS{conn: nc}, nil
 }
 
+// NewNATSWithCallback IMPORTANT! Not to send any stan.NatsURL or stan.SetConnectionLostHandler as options
+func NewNATSWithCallback(clusterID, clientID, url string, fn NatsCallback, options ...stan.Option) {
+	c := make(chan int)
+	options = append(options, stan.NatsURL(url))
+	options = append(options, stan.SetConnectionLostHandler(func(conn stan.Conn, reason error) {
+		log.Info("Nats connection lost!")
+
+		conn.Close()
+		c <- 1
+	}))
+	var nc stan.Conn
+	var err error
+
+	retryCount := 1
+	backoffer := &backoff.Backoff{
+		Min:    200 * time.Millisecond,
+		Max:    1 * time.Second,
+		Jitter: true,
+	}
+
+	for {
+		nc, err = stan.Connect(clusterID, clientID, options...)
+		if err != nil {
+			log.Info("Connect failed count: ", retryCount)
+			retryCount++
+
+			time.Sleep(backoffer.Duration())
+			continue
+		}
+
+		log.Info("Nats connection made...")
+		retryCount = 1
+
+		// Run callback function
+		fn(nc)
+
+		<-c
+	}
+}
+
 // NewTestNATS :nodoc:
 func NewTestNATS() *NATS {
 	return &NATS{testing: true}
+}
+
+// SetConn :nodoc:
+func (n *NATS) SetConn(conn stan.Conn) {
+	n.conn = conn
 }
 
 // Close NatsConnection :nodoc:
